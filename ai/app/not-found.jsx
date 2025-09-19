1671db231820bc5e66433b23be3509ef4e1eaf11
aryandeulkar <aryandeulkar921@gmail.com>
2025-09-19 23:56:28 +0530

    changed-ui-of-not-found-page

--- ai/app/not-found.jsx
+++ ai/app/not-found.jsx
@@ -1,6 +1,9 @@
+
+"use client";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { motion } from "framer-motion";
+
 
 export default function NotFound() {
   return (
